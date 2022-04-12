7c4760eca84ee02e56ca12fa8867e839a10258a6
Olafi-MooN <alefmastertutor@gmail.com>
2022-04-11 21:37:32 -0300

    first commit

---
+++ src/app/shared/components/statistical-card/statistical-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistical-card',
+  templateUrl: './statistical-card.component.html',
+  styleUrls: ['./statistical-card.component.css']
+})
+export class StatisticalCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
