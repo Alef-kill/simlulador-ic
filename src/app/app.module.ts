dedc3692db0f7645b26f91ceeb979b084b62bb8f
Olafi-MooN <alefmastertutor@gmail.com>
2022-04-11 21:57:05 -0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { CardComponent } from './shared/components/card/card.component';
+import { StatisticalCardComponent } from './shared/components/statistical-card/statistical-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    StatisticalCardComponent
   ],
   imports: [
     BrowserModule,
